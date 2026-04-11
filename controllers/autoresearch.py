322bd67c35b5c962631c25dd707b9fda6c7c788f
jkwonn <hellojustinkwon@gmail.com>
2026-04-11 21:38:27 +0000

    add roll compensation + feedforward from future_plan

--- controllers/autoresearch.py
+++ controllers/autoresearch.py
@@ -3,18 +3,33 @@ import numpy as np
 
 class Controller(BaseController):
   """
-  A simple PID controller
+  PID with roll compensation and feedforward from future_plan
   """
-  def __init__(self,):
-    self.p = 0.195
-    self.i = 0.100
-    self.d = -0.053
+  def __init__(self):
+    self.p = 0.3
+    self.i = 0.05
+    self.d = -0.1
     self.error_integral = 0
     self.prev_error = 0
 
   def update(self, target_lataccel, current_lataccel, state, future_plan):
-    error = (target_lataccel - current_lataccel)
+    # Roll compensation: subtract roll contribution from target
+    adjusted_target = target_lataccel - state.roll_lataccel
+    adjusted_current = current_lataccel - state.roll_lataccel
+
+    error = adjusted_target - adjusted_current
     self.error_integral += error
+    # Anti-windup
+    self.error_integral = np.clip(self.error_integral, -10, 10)
     error_diff = error - self.prev_error
     self.prev_error = error
-    return self.p * error + self.i * self.error_integral + self.d * error_diff
+
+    # PID output
+    pid_output = self.p * error + self.i * self.error_integral + self.d * error_diff
+
+    # Feedforward from future plan
+    ff = 0.0
+    if future_plan and len(future_plan.lataccel) > 0:
+      ff = future_plan.lataccel[0] * 0.3
+
+    return pid_output + ff
