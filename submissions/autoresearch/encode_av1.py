64d6e09c7c3195e2d52e6ea52ef3b4ca49e12d8e
jkwonn <hellojustinkwon@gmail.com>
2026-04-12 03:24:18 +0000

    exp14: CRF 32->30 for better quality

--- submissions/autoresearch/encode_av1.py
+++ submissions/autoresearch/encode_av1.py
@@ -34,7 +34,7 @@ def encode(input_path, output_path, scale, crf, preset, film_grain, keyint):
     out_stream.options = {
         'preset': str(preset),
         'crf': str(crf),
-        'svtav1-params': f'film-grain={film_grain}:keyint={keyint}:scd=0:variance-boost-strength=1',
+        'svtav1-params': f'film-grain={film_grain}:keyint={keyint}:scd=0',
     }
 
     n = 0
